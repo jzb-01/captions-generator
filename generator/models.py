f5f48c98f78710eb4e9e5ddce92e16235e2c8039
Jordan <jordanzarateb@gmail.com>
2026-05-22 23:37:10 -0500

    the final commit

--- generator/models.py
+++ generator/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class TemporaryTrack(models.Model):
     text = models.CharField(max_length=300, blank=True)
     start_time = models.CharField(max_length=12)
